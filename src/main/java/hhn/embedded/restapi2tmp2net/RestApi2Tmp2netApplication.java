ccf2e24c69c95b6775cbca1fb54aaa3b62de19bf
noteurt <rausch.pascal@gmx.de>
2022-11-05 11:17:09 +0100

    inital commit

---
+++ src/main/java/hhn/embedded/restapi2tmp2net/RestApi2Tmp2netApplication.java
@@ -0,0 +1,13 @@
+package hhn.embedded.restapi2tmp2net;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestApi2Tmp2netApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestApi2Tmp2netApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hhn/embedded/restapi2tmp2net/RestApi2Tmp2netApplicationTests.java
@@ -0,0 +1,13 @@
+package hhn.embedded.restapi2tmp2net;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApi2Tmp2netApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
